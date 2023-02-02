3b228b95e91dcd16d4d5264d4e7cbfd30ad531c4
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-02-02 13:21:24 -0300

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css';
+import {FC, useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App: FC = () => {
+  const [big, setBig] = useState<boolean>(false);
+
+  const createBoxes = (): JSX.Element[] => {
+    const boxes: JSX.Element[] = [];
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        boxes.push(
+          <div
+            className="box"
+            style={{ backgroundPosition: `${-j * 125}px ${-i * 125}px` }}
+            key={`${i}-${j}`}
+          />
+        );
+      }
+    }
+    return boxes;
+  };
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <button id="btn" className="magic" onClick={() => setBig(!big)}>
+        Magic 🎩
+      </button>
+      <div id="boxes" className={`boxes ${big ? "big" : ""}`}>
+        {createBoxes()}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
